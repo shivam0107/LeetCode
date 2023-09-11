28ef500ada788c02b45f1a5a1e6faadd611be70c
shivam singh <89995495+shivam0107@users.noreply.github.com>
2023-09-11 23:56:40 +0530

    Time: 2 ms (48.75%) | Memory: 6.2 MB (65.91%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        
+        if(s.length() != goal.length()){
+            return false;
+        }
+
+        s += s;
+       
+        
+        
+
+        if(s.find(goal) != -1) return true;
+        
+        return false;
+
+
+    }
+};
