8f10ed062129832030f6172b778de82273dce4d8
shivam singh <89995495+shivam0107@users.noreply.github.com>
2023-10-27 22:40:45 +0530

    Time: 3 ms (70.37%) | Memory: 10.1 MB (7.52%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+       int row = matrix.size();
+       int col = matrix[0].size();
+
+
+        //bruteforce O(n*m)
+        for(int i = 0; i<row;i++){
+            for(int j =0; j<col; j++){
+                if(matrix[i][j] == target){
+                    return true;
+                }
+            }
+        }
+       
+        
+        return 0;
+    }
+};
