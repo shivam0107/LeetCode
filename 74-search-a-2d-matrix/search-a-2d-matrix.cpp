c0819d46e8e3bd5c520366c68f77efb522dbac0d
shivam singh <89995495+shivam0107@users.noreply.github.com>
2023-10-27 23:00:14 +0530

    Time: 2 ms (71.31%) | Memory: 9.9 MB (15.21%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,16 +4,34 @@ public:
        int row = matrix.size();
        int col = matrix[0].size();
 
+       int low =0;
+       int high = (row * col) -1;
+
+       while(low <= high){
+           int mid = (low + high)/2;
+           int i = mid/col;
+           int j = mid%col;
+           if(matrix[i][j] == target){
+               return true;
+           }
+           else if(matrix[i][j] < target){
+               low = mid+1;
+           }
+           else{
+               high = mid-1;
+           }
+       }
+
 
         //bruteforce O(n*m)
-        for(int i = 0; i<row;i++){
-            for(int j =0; j<col; j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
-            }
-        }
-       
+        // for(int i = 0; i<row;i++){
+        //     for(int j =0; j<col; j++){
+        //         if(matrix[i][j] == target){
+        //             return true;
+        //         }
+        //     }
+        // }
+        
         
         return 0;
     }
