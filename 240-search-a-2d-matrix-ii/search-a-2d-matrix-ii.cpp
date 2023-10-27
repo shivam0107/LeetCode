86cb92165afe38fcfc54939114ad22c954877812
shivam singh <89995495+shivam0107@users.noreply.github.com>
2023-10-27 23:38:43 +0530

    Time: 87 ms (64.99%) | Memory: 15.2 MB (22.39%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -4,15 +4,34 @@ public:
 
         int row = matrix.size();
        int col = matrix[0].size();
-        
-        //bruteforce O(n*m)
-        for(int i = 0; i<row;i++){
-            for(int j =0; j<col; j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
+
+       //optimal solution -- O(n+m) , TC - O(1)
+
+       int i = 0;
+        int j = col-1;
+        while(i < row && j >= 0){
+            if(matrix[i][j] == target){
+                return true;
+            }
+            else if(matrix[i][j] > target){
+                j--;
+            }
+            else{
+                i++;
             }
         }
+
+       
+       //better approach  - O(n * log(m)) write it by your own
+        
+        //bruteforce O(n*m)
+        // for(int i = 0; i<row;i++){
+        //     for(int j =0; j<col; j++){
+        //         if(matrix[i][j] == target){
+        //             return true;
+        //         }
+        //     }
+        // }
         
         
         return 0;
