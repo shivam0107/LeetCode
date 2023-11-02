894a9bfb2e992b8123804b0cc2010325bc9644d2
shivam singh <89995495+shivam0107@users.noreply.github.com>
2023-11-02 18:22:31 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (63.53%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+       
+       if(n == 1) {
+           return true;
+       }else if(n == 0){
+           return false;
+       }
+
+       return  n%3 == 0 && isPowerOfThree(n/3);
+    }
+};
