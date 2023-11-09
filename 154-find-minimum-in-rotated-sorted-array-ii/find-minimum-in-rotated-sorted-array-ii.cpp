1ba6b3270fe01a73299624e26e57f7ff78c57294
shivam singh <89995495+shivam0107@users.noreply.github.com>
2023-11-09 22:42:35 +0530

    Time: 3 ms (82.74%) | Memory: 12.4 MB (88.16%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,13 +1,46 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int mini = INT_MAX;
-        for(int i = 0; i<nums.size(); i++){
-           if(nums[i] < mini){
-               mini = nums[i];
-           }
+        //optimal approach binary search -- O(n)
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        int low = 0;
+        int high = n-1;
+        int ans = INT_MAX;
+
+     
+        while(low <= high){
+            int mid = (low + high) /2;
+            
+             if(nums[low] < nums[mid]){
+                ans = min(ans , nums[low]);
+                low = mid+1;
+            }
+            else if(nums[low] == nums[mid]){
+                ans = min(ans , nums[low]);
+                low++;
+            }
+            else if(nums[mid] < nums[high]){
+
+                ans = min(ans , nums[mid]);
+                high  = mid-1;
+            }
+            else{
+                ans = min(ans , nums[mid]);
+                high--;
+            }
         }
 
-        return mini;
+
+
+        //brute Force approach -- O(N)
+        // int mini = INT_MAX;
+        // for(int i = 0; i<nums.size(); i++){
+        //    if(nums[i] < mini){
+        //        mini = nums[i];
+        //    }
+        // }
+
+        return ans;
     }
 };
