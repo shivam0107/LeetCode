3ba112ea91a0947807a0a622b4e1802ba5db67d9
shivam singh <89995495+shivam0107@users.noreply.github.com>
2023-11-24 22:14:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (67.13%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -50,6 +50,31 @@ public:
         }
     }
 
+    void solve2(int col , vector<int> &leftrow , vector<int> &upperDiagonal , vector<int> &lowerDiagonal  , vector<string> &board , vector<vector<string>> &ans , int n){
+        //base case
+        if(col == n){
+            ans.push_back(board);
+            return;
+        }
+
+        for(int row = 0; row<n; row++){
+            if(leftrow[row] == 0 && upperDiagonal[row+col] == 0 && lowerDiagonal[n-1 + col - row] == 0){
+                board[row][col] = 'Q';
+                leftrow[row] = 1;
+                upperDiagonal[row+col] = 1;
+                lowerDiagonal[n-1 + col - row] = 1;
+    solve2(col+1 , leftrow , upperDiagonal , lowerDiagonal , board , ans , n);
+                board[row][col] = '.';
+                leftrow[row] = 0;
+                upperDiagonal[row+col] = 0;
+                lowerDiagonal[n-1 + col - row] = 0;
+
+            }
+        }
+    }
+
+
+
 
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
@@ -61,7 +86,12 @@ public:
 
         }
 
-        solve(0 , board, ans , n);
+        vector<int> leftrow(n , 0) , upperDiagonal(2*n-1 , 0), lowerDiagonal(2*n-1 , 0);
+        
+
+        // solve(0 , board, ans , n);//take more complexity
+        //by hashing 
+        solve2(0 , leftrow , upperDiagonal , lowerDiagonal , board , ans , n);
         return ans;
     }
 };
