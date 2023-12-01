53ccdd4fb954948c824ba174fbb7a15f09e9bdb5
shivam singh <89995495+shivam0107@users.noreply.github.com>
2023-12-01 19:13:23 +0530

    Time: 4 ms (60.25%) | Memory: 9.6 MB (9.37%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,26 +3,54 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
         vector<int> ans;
+        unordered_map<int , int> mp;
+        stack<int> st;
+        int n = nums2.size();
 
-        map<int , int> mp;
-        for(int i = 0; i<nums2.size(); i++){
-            for(int j = i+1; j<nums2.size(); j++){
-                if(nums2[i] < nums2[j]){
-                    mp[nums2[i]] = nums2[j];
-                    break;
-                }
-            }
+       for(int i = n-1; i>=0; i--){
+           while(!st.empty() && st.top() < nums2[i]){
+               st.pop();
+           }
+
+           if(st.empty() == false){
+               mp[nums2[i]] = st.top();
+           }
+           else{
+               mp[nums2[i]] = -1;
+           }
+
+
+           st.push(nums2[i]);
         }
+
 
         for(int i = 0; i<nums1.size(); i++){
-            int x = mp[nums1[i]];
-            if(x != 0){
-                ans.push_back(x);
-            }
-            else{
-                ans.push_back(-1);
-            }
+                ans.push_back(mp[nums1[i]]);
         }
+
+
+
+
+        //using hashmap -- TC -- O(n^2)
+        // map<int , int> mp;
+        // for(int i = 0; i<nums2.size(); i++){
+        //     for(int j = i+1; j<nums2.size(); j++){
+        //         if(nums2[i] < nums2[j]){
+        //             mp[nums2[i]] = nums2[j];
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // for(int i = 0; i<nums1.size(); i++){
+        //     int x = mp[nums1[i]];
+        //     if(x != 0){
+        //         ans.push_back(x);
+        //     }
+        //     else{
+        //         ans.push_back(-1);
+        //     }
+        // }
         return ans;
     }
 };
