64c6bcad727930d30de2531887aec5684420248d
shivam singh <89995495+shivam0107@users.noreply.github.com>
2023-12-02 22:56:27 +0530

    Time: 3 ms (46.65%) | Memory: 7.1 MB (5.00%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -5,11 +5,11 @@ public:
         stack<char> st;
         for(int i = 0; i < s.length(); i++){
 
-            if(s[i] == '#' && !st.empty()){
-                st.pop();
-            }
-            else if(s[i] != '#'){
+            if(s[i] != '#'){
                 st.push(s[i]);
+            }
+            else if(s[i] == '#' && !st.empty()){
+                st.pop();
             }
         }
       string str;
