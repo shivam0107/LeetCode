fb46b92112461df7db0980e6d6212bf7e7fc6b45
shivam singh <89995495+shivam0107@users.noreply.github.com>
2023-12-02 22:54:28 +0530

    Time: 3 ms (46.65%) | Memory: 6.8 MB (23.18%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string backspace(string s){
+
+        stack<char> st;
+        for(int i = 0; i < s.length(); i++){
+
+            if(s[i] == '#' && !st.empty()){
+                st.pop();
+            }
+            else if(s[i] != '#'){
+                st.push(s[i]);
+            }
+        }
+      string str;
+
+
+        while(!st.empty()){
+            str += st.top();
+            st.pop();
+        }
+
+        return str;
+    }
+
+    bool backspaceCompare(string s, string t) {
+
+        string s1 = backspace(s);
+        string s2 = backspace(t);
+
+        if(s1 == s2){
+            return true;
+        }
+        
+        return false;
+        
+    }
+};
