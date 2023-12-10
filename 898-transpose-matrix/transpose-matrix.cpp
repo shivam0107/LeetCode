4ecfd6e67cebc6f135f4e582f3085e5c61c7e065
shivam singh <89995495+shivam0107@users.noreply.github.com>
2023-12-10 14:53:51 +0530

    Time: 11 ms (37.66%) | Memory: 11 MB (45.29%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.cpp
+++ 898-transpose-matrix/transpose-matrix.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
 
-        //Creating auxiliary matrix of size nXm... n=matrix[0].size and m=matrix.size()
-        vector<vector<int>> res(matrix[0].size(), vector<int>(matrix.size()));
+        int row = matrix.size();
+        int col = matrix[0].size();
         
-        for(int i=0;i<matrix.size();i++) {
-            for(int j=0;j<matrix[0].size();j++) {
-                res[j][i] = matrix[i][j];
+        vector<vector<int>> ans(col , vector<int>(matrix.size() , 0)) ;
+
+        for(int i = 0; i<matrix.size(); i++){
+            for(int j = 0; j<matrix[0].size(); j++){
+                ans[j][i] = matrix[i][j];
             }
         }
-        
-        return res;
+
+        return ans;
 
     }
 };
