48fe28c4d876bc7e55a65f33eb9c8e3d612945df
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-01-03 13:33:14 +0530

    Time: 44 ms (13.10%) | Memory: 19.5 MB (39.88%) - LeetSync

---
+++ 913-random-flip-matrix/random-flip-matrix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    set<pair<int , int>> s;
+    int n , m;
+    Solution(int m, int n) {
+      this->m = m;
+      this->n = n;
+      s.clear();
+    }
+    
+    vector<int> flip() {
+        int r = rand()%m;
+        int c = rand()%n;
+
+        while(s.count({r,c}) > 0){
+              r = rand()%m;
+              c = rand()%n;
+        }
+
+        s.insert({r , c});
+        return {r , c};
+    }
+    
+    void reset() {
+        s.clear();
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(m, n);
+ * vector<int> param_1 = obj->flip();
+ * obj->reset();
+ */
