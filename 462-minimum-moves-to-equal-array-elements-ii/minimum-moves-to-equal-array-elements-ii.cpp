bd9dfc307d45d85fc133d67d4df60a3f5834ef0c
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-01-07 22:10:49 +0530

    Time: 6 ms (91.24%) | Memory: 12.3 MB (31.63%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
@@ -12,12 +12,23 @@ public:
 
     int minMoves2(vector<int>& nums) {
         
-        long long ans = INT_MAX;
+        // long long ans = INT_MAX;
+        int n = nums.size();
+
+        sort(nums.begin() , nums.end());
+
+        int median = nums[n/2];
+        long long op = 0;
+
         for(int i = 0; i<nums.size(); i++){
-            ans = min(ans , findMin(nums , nums[i]));
+            op += abs(nums[i] - median);
         }
 
-        return (int)ans;
+        // for(int i = 0; i<nums.size(); i++){
+        //     ans = min(ans , findMin(nums , nums[i]));
+        // }
+
+        return (int)op;
 
     }
 };
