16cb2449d0061f731017b1b60345dd1d83c2585b
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-01-13 21:23:33 +0530

    Time: 42 ms (94.43%) | Memory: 16.8 MB (97.67%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -2,28 +2,52 @@ class Solution {
 public:
     int minSteps(string s, string t) {
 
-        map<char , int> mp1, mp2;
+    
+    // optimized solution
+    vector<int> v(26 , 0);
 
-        for(auto i : s){
-            mp1[i]++;
-        }
-        for(auto i : t){
-            mp2[i]++;
-        }
 
-        int cnt = 0;
+    int i = 0;
+    while(i < s.size()){
+        v[s[i] - 'a']++;
+        v[t[i] - 'a']--;
 
-       for(auto i : mp1){
-           if(mp2.find(i.first) != mp2.end()){
+        i++;
+    }
+
+    int ans = 0;
+    i = 0;
+    while(i < 26){
+        ans  += max(0 , v[i]);
+        i++;
+    }
+
+    return ans;
+
+
+
+        //brute force with extra space
+    //     map<char , int> mp1, mp2;
+    //     for(auto i : s){
+    //         mp1[i]++;
+    //     }
+    //     for(auto i : t){
+    //         mp2[i]++;
+    //     }
+
+    //     int cnt = 0;
+
+    //    for(auto i : mp1){
+    //        if(mp2.find(i.first) != mp2.end()){
               
-                   cnt += min(mp1[i.first] , mp2[i.first]);
+    //                cnt += min(mp1[i.first] , mp2[i.first]);
            
-           }
+    //        }
            
-       }
+    //    }
 
        
-        return s.size() - cnt;
+        // return s.size() - cnt;
         
     }
 };
