28570363edd863ce267561cc9017f6e79dec2183
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-01-21 12:40:03 +0530

    Time: 4 ms (25.45%) | Memory: 9.2 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //memoization 
 
     // int rec(vector<int> &nums , int n , vector<int> &dp){
     //     if(n < 0){
@@ -16,23 +17,43 @@ public:
     //     return dp[n] = max(x , y);
     // }
 
-    int rec(vector<int> &nums , int n , vector<int> &dp){
 
+//tabulation
+int rec(vector<int> &nums , int n , vector<int> &dp){
+    
         if(n == 1) return nums[0];
 
-       int prev2 = nums[0] ;
-       int prev = max(nums[0] , nums[1]);
-       int  curr = prev;
+        dp[0] = nums[0];
+        dp[1] = max(nums[0] , nums[1]);
 
-       for(int i =2; i<n; i++){
-          curr = max(prev , prev2+nums[i]);
-           prev2 = prev;
-           prev = curr;
-       }
+        for(int i = 2; i<n; i++){
+            dp[i] = max(dp[i-1] , dp[i-2] + nums[i]);
 
-       return curr;
-       
+        }
+
+        return dp[n-1];
     }
+
+
+
+    //space optimization
+    // int rec(vector<int> &nums , int n , vector<int> &dp){
+
+    //     if(n == 1) return nums[0];
+
+    //    int prev2 = nums[0] ;
+    //    int prev = max(nums[0] , nums[1]);
+    //    int  curr = prev;
+
+    //    for(int i =2; i<n; i++){
+    //       curr = max(prev , prev2+nums[i]);
+    //        prev2 = prev;
+    //        prev = curr;
+    //    }
+
+    //    return curr;
+       
+    // }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
