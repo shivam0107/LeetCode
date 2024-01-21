5263b53b2a18ee0aa98b8f95518f0fa9f1ff2b8a
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-01-21 12:34:56 +0530

    Time: 4 ms (25.45%) | Memory: 9.3 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,25 +1,43 @@
 class Solution {
 public:
 
+    // int rec(vector<int> &nums , int n , vector<int> &dp){
+    //     if(n < 0){
+    //         return 0;
+    //     }
+
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+
+    //     int x = nums[n] + rec(nums , n-2 , dp);
+    //     int y =  rec(nums , n-1 , dp);
+
+    //     return dp[n] = max(x , y);
+    // }
+
     int rec(vector<int> &nums , int n , vector<int> &dp){
-        if(n < 0){
-            return 0;
-        }
 
-        if(dp[n] != -1){
-            return dp[n];
-        }
+        if(n == 1) return nums[0];
 
-        int x = nums[n] + rec(nums , n-2 , dp);
-        int y =  rec(nums , n-1 , dp);
+       int prev2 = nums[0] ;
+       int prev = max(nums[0] , nums[1]);
+       int  curr = prev;
 
-        return dp[n] = max(x , y);
+       for(int i =2; i<n; i++){
+          curr = max(prev , prev2+nums[i]);
+           prev2 = prev;
+           prev = curr;
+       }
+
+       return curr;
+       
     }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n+1 , -1);
 
-       return rec(nums , n-1 , dp);
+       return rec(nums , n , dp);
     }
 };
