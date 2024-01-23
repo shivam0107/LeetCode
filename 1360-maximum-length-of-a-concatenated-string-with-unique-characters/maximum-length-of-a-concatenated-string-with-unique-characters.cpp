48a757e389b74a1b287cfc7867a6335f13b114ad
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-01-23 16:14:19 +0530

    Time: 233 ms (22.23%) | Memory: 114 MB (20.27%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,92 @@
+class Solution {
+public:
+
+    bool isUnique(string s){
+        unordered_map<char , int> mp;
+        for(int i = 0; i<s.length(); i++){
+            if(mp[s[i]] > 0){
+                return false;
+            }
+
+            mp[s[i]]++;
+            
+        }
+
+        return true;
+    }
+
+    int maxLen(int n , vector<string> &ans , vector<string> &arr){
+        if(n < 0 ){
+            return 0;
+        }
+
+        ans.push_back(arr[n]);
+        unordered_map<char , int> mp;
+         for(int i=0;i<ans.size();i++)
+        {
+            for(int j=0;j<ans[i].size();j++) mp[ans[i][j]]++;
+        }
+
+        int f = 1;
+        for(auto i : mp){
+            if(i.second > 1){
+                f = 0;
+                break;
+            }
+        }
+
+        int left = 0, right = 0;
+        if(f) left = arr[n].length() + maxLen(n-1 , ans , arr);
+        ans.pop_back();
+        right = maxLen(n-1 , ans , arr);
+
+        return max(left , right);
+    }
+
+
+    int maxLength(vector<string>& arr) {
+
+        //very bad solution bro 
+        //accha lo abhi optimized karta hu
+        int n = arr.size()-1;
+
+        vector<string> ans;
+        return maxLen(n , ans, arr);
+
+
+
+        // int length = 0;
+        // if(arr.size() == 1){
+        //     if(isUnique(arr[0])){
+        //         return arr[0].length();
+        //     }
+        // }
+
+        // for(int i = 0; i<arr.size(); i++){
+        //      string s = arr[i];
+
+        //      if(isUnique(arr[i])){
+        //         if(arr[i].length() > length){
+        //             length = arr[i].length();
+        //         }
+        //      }
+
+        //     for(int j = 0; j<arr.size(); j++){
+        //         s  +=  arr[j];
+        //         if(isUnique(s)){
+        //            if(s.length() > length){
+        //                length = s.length();
+        //            }
+        //         }else{
+        //             int n = arr[j].length();
+        //             int m = s.length();
+        //             s.erase(m-n  , m );
+        //             cout<<s<<endl;
+
+        //         }
+        //     }
+        // }
+
+        // return length;
+    }
+};
