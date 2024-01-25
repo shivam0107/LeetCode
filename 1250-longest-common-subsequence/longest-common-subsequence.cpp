51c39498c2b5a7e83551877f24cb3e1b167f2259
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-01-26 00:45:27 +0530

    Time: 26 ms (85.60%) | Memory: 25.7 MB (7.79%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -17,7 +17,7 @@ public:
 
     int fMem(int n , int m , string &t1 , string &t2 ,  vector<vector<int>> &dp){
         //base case
-        if(n < 0 || m < 0){
+        if(n  ==  0 || m  ==  0){
             return 0;
         }
 
@@ -25,13 +25,39 @@ public:
             return dp[n][m];
         }
 
-        if(t1[n] == t2[m]){
+        if(t1[n-1] == t2[m-1]){
            return dp[n][m] =  1+fMem(n-1 , m-1 , t1 , t2 , dp);
         }
 
        return dp[n][m] =  max(fMem(n , m-1 , t1 , t2 , dp) , fMem(n-1 , m , t1 , t2 , dp));
 
     }
+
+    int fTab(int n , int m , string &t1 , string &t2 ,  vector<vector<int>> &dp){
+        //base case
+
+        for(int i = 0; i<=n; i++) dp[i][0] = 0;
+        for(int j = 0; j<=m; j++) dp[0][j] = 0;
+
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                     if(t1[i-1] == t2[j-1]){
+                         dp[i][j] =  1+dp[i-1][j-1];
+                     }
+                     else{
+                         dp[i][j] =  max(dp[i][j-1] , dp[i-1][j]);
+                     }
+
+                
+            }
+        }
+
+        return dp[n][m];
+
+       
+
+    }
+
 
 
     int longestCommonSubsequence(string text1, string text2) {
@@ -40,7 +66,8 @@ public:
 
 
         // return f(n-1 , m-1 , text1 , text2);
-        vector<vector<int>> dp(n , vector<int>(m , -1));
-         return fMem(n-1 , m-1 , text1 , text2 , dp);
+        vector<vector<int>> dp(n+1 , vector<int>(m+1 , -1));
+        //  return fMem(n , m , text1 , text2 , dp);
+         return fTab(n , m , text1 , text2 , dp);
     }
 };
