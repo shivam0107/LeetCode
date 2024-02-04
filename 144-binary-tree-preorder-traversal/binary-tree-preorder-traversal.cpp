ad1fed3c22f6859047a288a4b8c44f1fede267f8
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-02-04 23:30:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (22.87%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -38,14 +38,54 @@ public:
         nums.push_back(temp->val);
     }
 
+    
  }
+
+  TreeNode* find(TreeNode* curr){
+        TreeNode* pred = curr->left;
+
+        while(pred->right != NULL && pred->right != curr){
+            pred = pred->right;
+        }
+
+        return pred;
+    }
+
+    void morrisTraversal(TreeNode* root , vector<int> &nums){
+        TreeNode* curr = root;
+        while(curr != NULL){
+           
+            if(curr->left == NULL){
+                 nums.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode* pred = find(curr);
+                if(pred->right == NULL){
+                    pred->right = curr;
+                     nums.push_back(curr->val);
+                    curr = curr->left;
+                }
+                else{
+                      
+                    pred->right = NULL;
+                   
+                    curr = curr->right;
+                  
+                }
+
+            }
+        }
+    }
+
+
 
 
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
         if(root == NULL) return ans;
 
-        iterativePreorder(root , ans);
+        morrisTraversal(root , ans);
 
         return ans;
     }
