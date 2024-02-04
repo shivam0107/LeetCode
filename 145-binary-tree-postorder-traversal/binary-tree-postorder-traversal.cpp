f1b7a14ecb013de85f111d339f83514bb4c2fd05
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-02-04 23:20:22 +0530

    Time: 4 ms (31.98%) | Memory: 9.9 MB (17.07%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -44,12 +44,79 @@ public:
  }
 
 
+    void singleStackPostOrder(TreeNode* root , vector<int> &num){
+        TreeNode* curr = root;
+        stack<TreeNode*> st;
+        while(curr != NULL || !st.empty()){
+            if(curr != NULL){
+                st.push(curr);
+                curr = curr->left;
+            }
+            else{
+                TreeNode* temp = st.top()->right;
+               
+                if(temp == NULL){
+                    temp = st.top();
+                    st.pop();
+
+                    num.push_back(temp->val);
+                    while(!st.empty() && temp == st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        num.push_back(temp->val);
+                    }
+                }
+                else{
+                    curr = temp;
+                }
+
+            }
+        }
+    }
+
+
+    
+    TreeNode* find(TreeNode* curr){
+        TreeNode* pred = curr->left;
+
+        while(pred->right != NULL && pred->right != curr){
+            pred = pred->right;
+        }
+
+        return pred;
+    }
+
+
+    void morrisTraversal(TreeNode* root , vector<int> &nums){
+        TreeNode* curr = root;
+        while(curr != NULL){
+            if(curr->left == NULL){
+                curr = curr->right;
+                
+            }
+            else{
+                TreeNode* pred = find(curr);
+                if(pred->right == NULL){
+                    pred->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    pred->right = NULL;
+                    nums.push_back(curr->val);
+                    curr = curr->right;
+                }
+
+            }
+        }
+    }
+
+
 
     vector<int> postorderTraversal(TreeNode* root) {
          vector<int> ans;
          if(root == NULL) return ans;
 
-        IterativePostorder(root , ans);
+        singleStackPostOrder(root , ans);
 
         return ans;
     }
