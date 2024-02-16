9387796a322678ce7a852616ac9bdb808bb99272
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-02-16 22:36:24 +0530

    Time: 136 ms (59.44%) | Memory: 66 MB (60.95%) - LeetSync

---
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        unordered_map<int , int> mp;
+        for(auto i : arr){
+            mp[i]++;
+        }
+
+        int cnt = 0;
+        vector<int> temp;
+
+        for(auto i : mp){
+            temp.push_back(i.second);
+
+        }
+
+        sort(temp.begin() , temp.end());
+
+        for(int i = 0; i<temp.size(); i++){
+             cout<<temp[i];
+        }
+        for(int i = 0; i< temp.size(); i++){
+          if (k > 0) {
+            if (temp[i] <= k) {
+                k -= temp[i];
+                temp[i] = 0;
+            } else {
+                temp[i] -= k;
+                k = 0;
+            }
+        } else {
+            break;
+        }
+
+           
+        }
+
+        for(int i = 0; i<temp.size(); i++){
+             if(temp[i] != 0) cnt++;
+        }
+        cout<<endl;
+        for(int i = 0; i<temp.size(); i++){
+             cout<<temp[i];
+        }
+
+        return cnt;
+
+        
+
+       
+    }
+};
