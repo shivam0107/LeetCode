4c856117b29465ce4e0d13d1d18bd2693ca42209
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-02-19 01:04:14 +0530

    Time: 34 ms (75.71%) | Memory: 16.1 MB (56.25%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -25,16 +25,35 @@ public:
 
     }
 
+      int solveTab(vector<int>& coins, int amount ){
+        //base case
+        vector<int> dp(amount+1 , INT_MAX);
+        dp[0] = 0;
+        
+        for(int i = 1; i<=amount; i++){
+                for(int j = 0; j<coins.size() ; j++){
+                    if(i-coins[j] >= 0 && dp[i-coins[j]] != INT_MAX )
+                    dp[i] = min(dp[i] , 1+dp[i-coins[j]]);
+                }
+        }
+
+        if(dp[amount] == INT_MAX) return -1;
+        return dp[amount];
+
+      }
+    
+
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
 
-        vector<int> dp(amount+1 , -1);
+    //     vector<int> dp(amount+1 , -1);
 
-       int ans =  solve(coins , amount , dp);
-       if(ans == INT_MAX){
-           return -1;
-       }
+    //    int ans =  solve(coins , amount , dp);
+    //    if(ans == INT_MAX){
+    //        return -1;
+    //    }
 
-       return ans;
+        return solveTab(coins , amount);
+
     }
 };
