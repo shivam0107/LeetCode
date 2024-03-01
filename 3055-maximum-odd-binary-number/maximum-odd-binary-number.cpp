03783043ab96a8f5513e59814e12bcb1b7349cd1
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-03-01 22:18:58 +0530

    Time: 3 ms (77.44%) | Memory: 8.2 MB (68.48%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -2,22 +2,30 @@ class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
         
-        int j = 0;
-       
+        int l = s.length()-1;
+        int m  = 0;
+       int cnt = 1;
 
         for(int i = 0; i<s.length(); i++){
-            if(s[i] == '1'){
-                swap(s[i] ,s[j] );
-                j++;
+            if(s[i] == '1' && cnt == 1){
+                swap(s[i] ,s[l] );
+               cnt = 0;
             }
+
+           if(i != s.length()-1){
+                if(s[i] == '1'){
+                swap(s[i] , s[m]);
+                m++;
+            }
+           }
         }
 
-        for(int i = 0; i<s.length(); i++){
-            if(i+1 < s.length() && s[i+1] == '0' ){
-                swap(s[i] , s[s.length()-1]);
-                break;
-            }
-        }
+        // for(int i = 0; i<s.length(); i++){
+        //     if(i+1 < s.length() && s[i+1] == '0' ){
+        //         swap(s[i] , s[s.length()-1]);
+        //         break;
+        //     }
+        // }
 
 
         return s;
