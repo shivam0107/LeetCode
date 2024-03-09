8bb2143aaec0239168c7aef35c894d09219a4775
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-03-09 17:58:47 +0530

    Time: 103 ms (24.97%) | Memory: 69.2 MB (12.38%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -6,12 +6,26 @@ public:
 
         unordered_map<int , int> mp;
         for(auto i : num2){
-            mp[i]++;
+            mp[i ]++;
         }
 
-        for(int i = 0; i<n; i++){
-            if(mp.find(num1[i]) != mp.end() ){
+        // for(int i = 0; i<n; i++){
+        //     if(mp.find(num1[i]) != mp.end() ){
+        //         return num1[i];
+        //     }
+        // }
+
+        int i = 0;
+        int j = 0;
+        while(i < n && j < m){
+            if(num1[i] == num2[j]){
                 return num1[i];
+            }
+            else if(num1[i] < num2[j]){
+                i++;
+            }
+            else{
+                j++;
             }
         }
 
