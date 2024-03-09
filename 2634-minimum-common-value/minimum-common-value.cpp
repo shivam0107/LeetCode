159ba89244d4c0bf2720f75138377763e4bbc42a
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-03-09 17:59:58 +0530

    Time: 57 ms (95.58%) | Memory: 52.9 MB (51.54%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -3,11 +3,12 @@ public:
     int getCommon(vector<int>& num1, vector<int>& num2) {
         int n = num1.size();
         int m = num2.size();
-
-        unordered_map<int , int> mp;
-        for(auto i : num2){
-            mp[i ]++;
-        }
+        
+        //brute force
+        // unordered_map<int , int> mp;
+        // for(auto i : num2){
+        //     mp[i ]++;
+        // }
 
         // for(int i = 0; i<n; i++){
         //     if(mp.find(num1[i]) != mp.end() ){
