1436c03838a863cec6349d9bd539572eedc4919b
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-03-10 12:12:52 +0530

    Time: 3 ms (83.79%) | Memory: 13.4 MB (18.03%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+        map<int ,int> map1;
+        map<int ,int> map2;
+
+        for(auto i : nums1){
+            map1[i]++;
+        }
+
+         for(auto i : nums2){
+            map2[i]++;
+        }
+
+        vector<int> ans;
+
+
+
+        for(auto i : map1){
+            if(map2.find(i.first) != map2.end()){
+                ans.push_back(i.first);
+            }
+        }
+
+        return ans;
+
+
+        
+
+        
+    }
+};
