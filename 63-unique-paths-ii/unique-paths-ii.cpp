86cf427c201e0b38b15d632d26aef6d1c3ec4f7c
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-03-13 01:06:10 +0530

    Time: 6 ms (20.08%) | Memory: 10.3 MB (31.68%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -13,8 +13,6 @@ public:
         int l = 0;
         int r = 0;
 
-
-
         if(grid[i][j] != 1){
             l = f(i-1 , j , grid , dp);
         }
@@ -25,12 +23,41 @@ public:
         return dp[i][j] = l+r;
     }
 
+     int fTab(int m , int n ,vector<vector<int>>& grid){
+        vector<vector<int>> dp(m+1 , vector<int>(n+1 , 0));
+
+        for(int i = 0; i<=m; i++){
+            for(int j = 0; j<= n; j++){
+
+                if(i == 0 && j == 0 && grid[i][j] != 1){
+                      dp[0][0] = 1;
+                }
+                else{
+                      int up = 0;
+                      int left = 0;
+                      if(i > 0 && grid[i][j] != 1){
+                          up = dp[i-1][j] ;
+                         
+                      }
+                      if(j > 0 && grid[i][j] != 1){
+                          left = dp[i][j-1];
+                      }
+
+
+                      dp[i][j] = up+left;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
         vector<vector<int>> dp(m , vector<int>(n , -1));
 
-        return f(m-1 , n-1 , grid , dp);
+        // return f(m-1 , n-1 , grid , dp);
+        return fTab(m-1 , n-1 , grid);
     }
 };
