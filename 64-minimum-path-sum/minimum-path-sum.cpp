ee0d5c15a651d360cabdf7eb9aadf50ce12704c5
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-03-13 01:38:29 +0530

    Time: 6 ms (64.86%) | Memory: 12.8 MB (12.40%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -17,6 +17,36 @@ public:
 
     }
 
+     int fTab(int m , int n ,vector<vector<int>>& grid){
+         vector<vector<int>> dp(m+1 , vector<int>(n+1 , INT_MAX));
+        
+          for(int i = 0; i<=m; i++){
+            for(int j = 0; j<= n; j++){
+
+                if(i == 0 && j == 0){
+                      dp[0][0] = grid[0][0];
+                }
+                else{
+                      int up = INT_MAX;
+                      int left = INT_MAX;
+                      if(i > 0){
+                          up = grid[i][j] + dp[i-1][j] ;
+                         
+                      }
+                      if(j > 0){
+                          left = grid[i][j] + dp[i][j-1];
+                      }
+
+
+                      dp[i][j] = min(up , left);
+                }
+            }
+        }
+        return dp[m][n];
+
+
+    }
+
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
@@ -25,6 +55,7 @@ public:
 
 
 
-        return f(m-1 , n-1 , grid , dp);
+        // return f(m-1 , n-1 , grid , dp);
+        return fTab(m-1 , n-1 , grid);
     }
 };
