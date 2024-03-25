fad3f68f203dd6e4df1bc42864d03647440becfb
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-03-25 21:33:21 +0530

    Time: 75 ms (22.95%) | Memory: 48 MB (32.11%) - LeetSync

---
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        unordered_map<int , int>     mp;
+
+        for(auto i : nums){
+            mp[i]++;
+        }
+
+        for(auto i : mp){
+            if(i.second >= 2 ){
+                ans.push_back(i.first);
+            }
+        }
+
+
+        return ans;
+
+    }
+};
