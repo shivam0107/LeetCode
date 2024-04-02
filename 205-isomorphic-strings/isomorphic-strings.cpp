71ee653cb228980d0341ef0ff48dbd3254981834
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-04-02 11:15:20 +0530

    Time: 7 ms (48.84%) | Memory: 8.2 MB (85.95%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -4,21 +4,26 @@ public:
             if(s.length() != t.length()){
                 return false;
             }
-            
+            //map string s to string t
             unordered_map<char , char> map1;
+
+            // map target string t to bool weather it is used or not
             unordered_map<char , bool> map2;
 
             for(int i = 0; i<s.length() ; i++){
 
                 char ch1  = s[i];
                 char ch2 =  t[i];
-
+                
+                //check for ch1 is used or not
                 if(map1.find(ch1) != map1.end()){
+                    //if ch1 is used check for mapping to ch2
                     if(map1.at(ch1) != ch2){
                         return false;
                     }
                 }
                 else{
+                    // if not mapped thep map ch1 with ch2
                     if(map2.find(ch2) != map2.end()){
                         return false;
                     }
