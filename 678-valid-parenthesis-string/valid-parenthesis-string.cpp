83943bee5e0c3e9ce2f4537806df9a0385550aec
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-04-07 12:03:36 +0530

    Time: 4 ms (9.36%) | Memory: 7.1 MB (99.47%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int open = 0 , close = 0;
+
+        for(auto i : s){
+            if(i == ')'){
+                open--;
+            }
+            else{
+                open++;
+            }
+
+            cout<<open<<endl;
+            if(open < 0) return false;
+        }
+
+        for(int i = s.length()-1; i>=0; i--){
+            if(s[i] == '('){
+                close--;
+            }
+            else{
+                close++;
+            }
+
+            if(close < 0) return false;
+        }
+
+        return true;
+    }
+};
