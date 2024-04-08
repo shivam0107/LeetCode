ff4f518e181ed8fd10ff3462f4b4349dee75f0d5
shivam singh <89995495+shivam0107@users.noreply.github.com>
2024-04-08 23:27:18 +0530

    Time: 5 ms (31.29%) | Memory: 10.9 MB (22.31%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -3,33 +3,27 @@ public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         
         queue<int> q1;
-        queue<int> q2;
 
-        int count = 0;
+        int count = 0 , i = 0;
 
         for(auto i : students){
             q1.push(i);
         }
-
-        for(auto i : sandwiches){
-            q2.push(i);
-        }
-
-        while(!q1.empty() && !q2.empty()){
+        while(!q1.empty()){
 
             if(count > q1.size()) return q1.size();
 
-            if(q1.front() != q2.front()){
+            if(q1.front() != sandwiches[i]){
                 int n = q1.front();
                 q1.pop();
             
                 q1.push(n);
                 count++;
-                cout<<count;
+            
             }
             else{
                 q1.pop();
-                q2.pop();
+                i++;   
                 count = 0;
             }
 
